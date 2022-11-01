7a435a35e99cdb08ac7bc24cca48b6c7850dc8c1
Jelle Keulemans <jelle.keulemans@axxes.com>
2022-11-01 15:49:06 +0100

    working sin wave

---
+++ angular-wave/src/app/app.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  animationFrameScheduler,
+  BehaviorSubject,
+  map,
+  Observable,
+  bufferCount,
+  observeOn,
+  of,
+  Subscriber,
+  pipe,
+} from 'rxjs';
+
+const ITEM_COUNT = 2000;
+const STEP_SIZE = 0.1;
+const BAR_SIZE = 0.01;
+
+let previousFps: number[] = [
+  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+];
+const averageFps = (fps: number) => {
+  previousFps.push(fps);
+  previousFps = previousFps.slice(1);
+  return Math.round(
+    previousFps.reduce((p, c) => {
+      if (p === 0) return c;
+      return (p + c) / 2;
+    }, 0)
+  );
+};
+
+const getFrameEmitter: () => Observable<[number, number]> = () => {
+  return new Observable((subscriber) => {
+    let i = 0;
+    let id = NaN;
+
+    let previousFrame = Date.now();
+
+    const step = () => {
+      const currentFrame = Date.now();
+      subscriber.next([i++, currentFrame - previousFrame]);
+      previousFrame = currentFrame;
+      id = requestAnimationFrame(step);
+    };
+    id = requestAnimationFrame(step);
+
+    return () => {
+      cancelAnimationFrame(id);
+    };
+  });
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  frames$ = getFrameEmitter();
+
+  values$ = this.frames$.pipe(
+    map(([index]) => index * STEP_SIZE),
+    map((offset) => {
+      return Array.from({ length: ITEM_COUNT }, (_, i) =>
+        Math.floor((Math.sin(offset + i * BAR_SIZE) + 1) * 50)
+      );
+    })
+  );
+
+  fps$ = this.frames$.pipe(
+    map(([_, interval]) => Math.round(10000 / interval) / 10),
+    bufferCount(10),
+    map((buffer) =>
+      buffer.reduce((p, c) => {
+        if (p === 0) return c;
+        return (p + c) / 2;
+      }, 0)
+    )
+  );
+
+  // values$ = this.animationFrame$.pipe(
+  //   map(() => {
+  //     this.offset += STEP_SIZE;
+  //     return Array.from({ length: ITEM_COUNT }, (_, i) =>
+  //       Math.floor((Math.sin(this.offset + i * BAR_SIZE) + 1) * 50)
+  //     );
+  //   })
+  // );
+}
