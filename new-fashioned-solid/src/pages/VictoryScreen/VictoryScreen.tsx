abb450ce6684d97f6d3e1b4f87e712f4ed291ce8
Jelle Keulemans <jelle.keulemans@axxes.com>
2022-11-09 14:29:49 +0100

    fix tsconfig

--- new-fashioned-solid/src/pages/VictoryScreen/VictoryScreen.tsx
+++ new-fashioned-solid/src/pages/VictoryScreen/VictoryScreen.tsx
@@ -1,7 +1,7 @@
 const VictoryScreen = () => {
   return (
-    <div className="h-screen w-screen bg-black flex items-center justify-center">
-      <h1 className="text-center inline-block p-8 bg-green-100 text-7xl rounded">
+    <div class="h-screen w-screen bg-black flex items-center justify-center">
+      <h1 class="text-center inline-block p-8 bg-green-100 text-7xl rounded">
         Game ended
       </h1>
     </div>
